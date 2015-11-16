3ece0fa2b956c86c65b2dc70d76c0e23d6bae884
Andreas Knops <aknops@roamsys.com>
2015-11-16 10:48:30 +0100

    Log-Ausgabe geändert.

--- MyProject/src/main/java/lu/roamsys/MyPackage/App.java
+++ MyProject/src/main/java/lu/roamsys/MyPackage/App.java
@@ -18,7 +18,7 @@ public class App
         
         BasicConfigurator.configure();
         logger.debug("Hello World!");
-        logger.debug("Hello World!");
+        logger.debug("Hello Franz!");
         
     }
 }
