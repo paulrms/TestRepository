c6a443b81ba089f663e39f26b77740180301323a
Andreas Knops <aknops@roamsys.com>
2015-11-16 09:42:38 +0100

    Initialer Commit

---
+++ MyProject/src/main/java/lu/roamsys/HibernateTest/App.java
@@ -0,0 +1,24 @@
+package lu.roamsys.HibernateTest;
+
+import org.apache.log4j.BasicConfigurator;
+import org.apache.log4j.Logger;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+	static final Logger logger = Logger.getLogger(App.class);
+	
+	
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+        
+        BasicConfigurator.configure();
+        logger.debug("Hello World!");
+        
+        
+    }
+}
