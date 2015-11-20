7287fdc704f7926f221928ed4f6105404f65580c
mlossbrand <mlossbrand@10.10.3.94>
2015-11-20 16:19:07 +0100

    Servus

--- MyProject/src/main/java/lu/roamsys/MyPackage/App.java
+++ MyProject/src/main/java/lu/roamsys/MyPackage/App.java
@@ -19,7 +19,7 @@ public class App
     	// print greetings to the console
         System.out.println( "Hello World!" );
         
-        System.out.println("Hi!");
+        System.out.println("Servus!");
         
         BasicConfigurator.configure();
         logger.debug("Hello World!");
